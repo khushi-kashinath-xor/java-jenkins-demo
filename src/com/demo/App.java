471ce749190be135a8ac7fba19ed9775dfee59ba
Khushi Kashinath <Khushi.Kashinath@xoriant.com>
2026-01-20 16:05:07 +0530

    changed to test webhook

--- src/com/demo/App.java
+++ src/com/demo/App.java
@@ -4,5 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Jenkins! Java build successful 🚀");
+        System.out.println("Hello ...checking");
     }
 }
